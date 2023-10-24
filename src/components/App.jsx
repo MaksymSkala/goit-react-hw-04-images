c955fe7c1bd4e39343f10a3bbe93d53ccc7efcbb
MaksymSkala <maksymskala@gmail.com>
2023-10-24 22:38:00 +0300

    Add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,31 +6,41 @@ import Loader from './Loader/Loader';
 import { fetchImages } from '../Api/api';
 import './App.css';
 
-function App() {
+const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [largeImageURL, setLargeImageURL] = useState('');
   const [hasMoreImages, setHasMoreImages] = useState(true);
+  const [moreImagesAvailable, setMoreImagesAvailable] = useState(true);
 
   useEffect(() => {
-    if (searchQuery !== '') {
-      setLoading(true);
-      fetchImages(searchQuery, page)
-        .then((data) => {
-          if (data.hits.length === 0) {
-            setHasMoreImages(false);
-          } else {
-            setImages((prevImages) => [...prevImages, ...data.hits]);
-            if (data.hits.length < 12) {
-              setHasMoreImages(false);
-            }
-          }
-        })
-        .catch((error) => setError(error))
-        .finally(() => setLoading(false));
+    if (searchQuery.trim() === '') {
+      return;
     }
+
+    if (page === 1) {
+      setImages([]);
+    }
+
+    setLoading(true);
+
+    fetchImages(searchQuery, page)
+      .then((data) => {
+        if (data.hits.length === 0) {
+          setHasMoreImages(false);
+        } else {
+          setImages((prevImages) => [...prevImages, ...data.hits]);
+          if (data.hits.length < 12) {
+            setHasMoreImages(false);
+            setMoreImagesAvailable(false);
+          }
+        }
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, [searchQuery, page]);
 
   const handleSearchFormSubmit = (query) => {
@@ -38,6 +48,7 @@ function App() {
     setPage(1);
     setImages([]);
     setHasMoreImages(true);
+    setMoreImagesAvailable(true);
   };
 
   const handleImageClick = (largeImageURL) => {
@@ -48,6 +59,12 @@ function App() {
     setLargeImageURL('');
   };
 
+  const handleLoadMoreClick = () => {
+    if (moreImagesAvailable) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
     <div>
       <Searchbar onSubmit={handleSearchFormSubmit} />
@@ -55,16 +72,21 @@ function App() {
       {images.length > 0 && (
         <ImageGallery images={images} onClick={handleImageClick} />
       )}
-      {hasMoreImages && !loading && images.length > 0 && (
-        <button type="button" className="button" onClick={() => setPage(page + 1)}>
-          Load more
-        </button>
-      )}
       {largeImageURL && (
         <Modal image={largeImageURL} onClose={handleCloseModal} />
       )}
+      {hasMoreImages && images.length > 0 && (
+        <button
+          type="button"
+          className="button"
+          onClick={handleLoadMoreClick}
+        >
+          Load more
+        </button>
+      )}
+      {error && <p>Error: {error.message}</p>}
     </div>
   );
-}
+};
 
 export default App;
